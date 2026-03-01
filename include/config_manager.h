815221dd94302934155e867163d153c10816693d
Aidre Cabrera <aidrecabrera@gmail.com>
2026-03-01 23:30:10 +0800

    config_manager: declare NVS read/write interface

---
+++ include/config_manager.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "types.h"
+
+namespace ConfigManager {
+
+bool init();
+
+bool loadDefaults();
+
+const RuntimeConfig& getConfig();
+
+bool saveThresholds(const ThresholdConfig& thresholds);
+
+bool saveMode(DeviceMode mode);
+
+bool saveLedState(const LedState& led);
+
