047281229ce0589b6c6cd5dd83e5bb4f15fceaef
Aidre Cabrera <aidrecabrera@gmail.com>
2026-03-02 00:33:34 +0800

    mqtt_client: declare broker session and pub/sub interface

---
+++ include/mqtt_client.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "types.h"
+
+typedef void (*MqttInboundCallback)(
+    const char* topic,
+    const uint8_t* payload,
+    uint16_t length
+);
+
+namespace MqttClient {
+
+bool init(const char* device_id);
+
+bool connectOrPoll();
+
+bool publishTelemetry(const SensorReading& reading);
+
+bool publishStatus(const StatusMessage& status);
+
+bool publishEnergy(const EnergyMessage& energy);
+
+bool publishAck(const AckMessage& ack);
+
+bool setupSubscriptions();
+
+void registerInboundCallback(MqttInboundCallback callback);
+
+bool isConnected();
+
+}
