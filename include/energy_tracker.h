bcf7c530c63a1a5ce274ac61df148d5ba261384f
Aidre Cabrera <aidrecabrera@gmail.com>
2026-03-02 23:00:02 +0800

    energy_tracker: declare accumulation and persistence interface

---
+++ include/energy_tracker.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "types.h"
+
+namespace EnergyTracker {
+
+bool init();
+
+void updateFromLedState(const LedState& led);
+
+EnergyMessage getSnapshot();
+
+bool requestPersist();
+
+}
