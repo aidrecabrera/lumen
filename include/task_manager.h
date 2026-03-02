01f9bfcd2e58da56b3653be533d3b7495d064b4a
Aidre Cabrera <aidrecabrera@gmail.com>
2026-03-02 23:13:19 +0800

    task_manager: declare FreeRTOS queues and task handles

---
+++ include/task_manager.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "types.h"
+
+#include <freertos/FreeRTOS.h>
+#include <freertos/queue.h>
+#include <freertos/task.h>
+
+extern QueueHandle_t sensor_to_led_queue;
+
+extern QueueHandle_t status_to_mqtt_queue;
+
+extern QueueHandle_t ack_to_mqtt_queue;
+
+namespace TaskManager {
+
+bool createQueues();
+
+bool createTasks();
+
+}
