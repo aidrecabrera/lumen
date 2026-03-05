a5f05fa66a514223068ac6ad6cd7ef3664670fd3
Aidre Cabrera <aidrecabrera@gmail.com>
2026-03-05 22:32:38 +0800

    task_manager: add missing queue externs so translation units can access shared handles

--- include/task_manager.h
+++ include/task_manager.h
@@ -6,9 +6,15 @@
 #include <freertos/queue.h>
 #include <freertos/task.h>
 
+extern QueueHandle_t sensor_to_mqtt_queue;
+
 extern QueueHandle_t sensor_to_led_queue;
 
+extern QueueHandle_t command_dispatch_queue;
+
 extern QueueHandle_t status_to_mqtt_queue;
+
+extern QueueHandle_t energy_to_mqtt_queue;
 
 extern QueueHandle_t ack_to_mqtt_queue;
 
